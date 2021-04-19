9677c1991bc7713db1622fb96563e1c284d75fab
aditya <adityasu12@gmail.com>
2021-04-19 19:01:07 +0530

    added design to web page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
-# import sounddevice as sd
-# import soundfile as sf
+import sounddevice as sd
+import soundfile as sf
 import os
-import model_run
+import onnxruntime as ort
+import librosa
 
 app = Flask(__name__)
+classes = ['down', 'no', 'left', 'go', 'off', 'on', 'right', 'stop', 'up', 'yes']
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -12,43 +14,53 @@ def index():
     return render_template('index.html', preds=preds)
 
 
-# def record():
-#     sr = 16000  # Sample rate
-#     seconds = 1  # Duration of recording
-#     print('recording')
-#     myrecording = sd.rec(int(seconds * sr), samplerate=sr, channels=1, blocking=True)
-#     print('done')
-#     sd.wait()  # Wait until recording is finished
-#     if os.path.exists('output.wav'):
-#         os.remove('output.wav')
-#         print('removed')
-#     else: 
-#         print('writing 1st time')
-#     sf.write('output.wav', myrecording, sr)
-#     return True
+def run_model():
+    print('run model')
+    samples, sample_rate = librosa.load('output.wav', sr=16000)
+    samples = librosa.resample(samples, sample_rate, 8000)
+    ort_session = ort.InferenceSession('trained_model.onnx')
+    inputs = {ort_session.get_inputs()[0].name: samples.reshape(1,8000,1)}
+    output = ort_session.run(None, inputs)[0].argmax()
+    print(output, classes[output])
+    return classes[output]
+
+def record():
+    sr = 16000  # Sample rate
+    seconds = 1  # Duration of recording
+    print('recording')
+    myrecording = sd.rec(int(seconds * sr), samplerate=sr, channels=1, blocking=True)
+    print('done')
+    sd.wait()  # Wait until recording is finished
+    if os.path.exists('output.wav'):
+        os.remove('output.wav')
+        print('removed')
+    else: 
+        print('writing 1st time')
+    sf.write('output.wav', myrecording, sr)
+    return True
 
 
 def play():
     print('playing')
-    # data, fs = sf.read('output.wav')
-    # sd.play(data, fs)
-    # status = sd.wait()
-    # print(status)
+    data, fs = sf.read('output.wav')
+    sd.play(data, fs)
+    status = sd.wait()
+    print(status)
 
 
 @app.route('/start', methods=['GET','POST'])
 def start():
-    # print(record()) 
+    print(record()) 
     play()
     return redirect('/')
 
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
-    preds = model_run.run_model()
+    preds = run_model()
     
     return render_template('index.html', preds='prediction = '+preds)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(port=port)
+    app.run(debug=True, port=port)
