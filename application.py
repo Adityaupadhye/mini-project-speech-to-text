bf1f1cffa08a7ffb01560b5bad186c5e814c7ab4
aditya <adityasu12@gmail.com>
2021-04-24 17:40:38 +0530

    add training ipynb file, remove comments from app.py

--- application.py
+++ application.py
@@ -41,52 +41,6 @@ def record():
     preds = classes[output]
     return 'prediction = '+preds
 
-# def run_model():
-#     print('run model')
-#     samples, sample_rate = librosa.load('output.wav', sr=16000)
-#     print('samples = ', samples, type(samples), np.shape(samples))
-#     samples = librosa.resample(samples, sample_rate, 8000)
-#     print('sample shape = ', np.shape(samples))
-#     inputs = {ort_session.get_inputs()[0].name: samples.reshape(1,8000,1)}
-#     output = ort_session.run(None, inputs)[0].argmax()
-#     print(output, classes[output])
-#     return classes[output]
-
-# print('done')
-# sd.wait()  # Wait until recording is finished
-# if os.path.exists('output.wav'):
-#     os.remove('output.wav')
-#     print('removed')
-# else: 
-#     print('writing 1st time')
-# sf.write('output.wav', myrecording, sr)
-# return True
-
-
-# def play():
-#     print('playing')
-#     data, fs = sf.read('output.wav')
-#     sd.play(data, fs)
-#     status = sd.wait()
-#     print(status)
-
-
-# @app.route('/start', methods=['GET','POST'])
-# def start():
-#     print(record()) 
-#     play()
-#     preds = run_model()
-    
-#     return render_template('index.html', preds='prediction = '+preds)
-#     return redirect('/')
-
-
-# @app.route('/predict', methods=['GET','POST'])
-# def predict():
-#     preds = run_model()
-    
-#     return render_template('index.html', preds='prediction = '+preds)
-
 if __name__ == '__main__':
     ui.run()
     # app.run(debug=True)
